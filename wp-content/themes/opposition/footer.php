749a7cfcd7c9a4f99dfeeb49fc0eacac07960f10
andrewyamamoto <andrew.asada@gmail.com>
2016-08-05 17:07:32 -0700

    added plugins and pages

--- wp-content/themes/opposition/footer.php
+++ wp-content/themes/opposition/footer.php
@@ -13,13 +13,7 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'opposition' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'opposition' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'opposition' ), 'opposition', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
---
+++ wp-content/themes/opposition/template-parts/content-intro.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package opposition
+ */
+
+?>
+<section id="intro">
+    <?php get_template_part('nav'); ?>
+</section>
