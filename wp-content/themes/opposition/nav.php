fcca34c93eda5bb800146c331dec16cdb248180b
andrewyamamoto <andrew.asada@gmail.com>
2016-08-08 18:55:24 -0700

    Nav edits

--- wp-content/themes/opposition/nav.php
+++ wp-content/themes/opposition/nav.php
@@ -9,14 +9,17 @@
     <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header">
-                <img src="<?php echo get_template_directory_uri() . '/img/opposition_logo.png'?>" width='50' alt="" />
+                <!-- <a href="#"></a> -->
 
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#oppo-navbar-collapse" aria-expanded="false">
                    <span class="icon-bar"></span>
                    <span class="icon-bar"></span>
                    <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">OPPOSITION</a>
+                <a class="navbar-brand" href="/">
+                    <img src="<?php echo get_template_directory_uri() . '/img/opposition_logo.png'?>" width='50' alt="" />
+                    OPPOSITION
+                </a>
             </div>
             <div class="collapse navbar-collapse" id="oppo-navbar-collapse">
                 <?php
