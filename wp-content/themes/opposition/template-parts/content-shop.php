1a6a428f821f274e5562274cfb9c98c9257ba009
andrewyamamoto <andrew.asada@gmail.com>
2016-08-14 20:17:09 -0700

    uncommented shop

--- wp-content/themes/opposition/template-parts/content-shop.php
+++ wp-content/themes/opposition/template-parts/content-shop.php
@@ -34,40 +34,40 @@
             <div class="col-lg-12">
                 <?php
 
-                    // echo do_shortcode( '[featured_products per_page="12" columns="4" orderby="date" ]' );
-                    //
-                    // $args = array(
-                    //     'post_type' => 'product',
-                    //     'meta_key' => '_featured',
-                    //     'meta_value' => 'yes',
-                    //     'posts_per_page' => 12
-                    // );
-                    //
-                    // $featured_query = new WP_Query( $args );
-                    //
-                    // if ($featured_query->have_posts()) :
-                    //
-                    //     while ($featured_query->have_posts()) :
-                    //
-                    //         $featured_query->the_post();
-                    //
-                    //         $product = get_product( $featured_query->post->ID );
-                    //         $price = get_post_meta( $featured_query->post->ID, '_regular_price', true);
-                    //         $addtocart = get_post_meta( $featured_query->post->ID, '_regular_price', true);
-                    //         print_r($product);
-                    //         echo "$price";
-                    //         // foreach($product as $item){
-                    //         //
-                    //         // }
-                    //
-                    //         // Output product information here
-                    //
-                    //
-                    //     endwhile;
-                    //
-                    // endif;
-                    //
-                    // wp_reset_query();
+                    echo do_shortcode( '[featured_products per_page="12" columns="4" orderby="date" ]' );
+
+                    $args = array(
+                        'post_type' => 'product',
+                        'meta_key' => '_featured',
+                        'meta_value' => 'yes',
+                        'posts_per_page' => 12
+                    );
+
+                    $featured_query = new WP_Query( $args );
+
+                    if ($featured_query->have_posts()) :
+
+                        while ($featured_query->have_posts()) :
+
+                            $featured_query->the_post();
+
+                            $product = get_product( $featured_query->post->ID );
+                            $price = get_post_meta( $featured_query->post->ID, '_regular_price', true);
+                            $addtocart = get_post_meta( $featured_query->post->ID, '_regular_price', true);
+                            // print_r($product);
+                            echo "$price";
+                            // foreach($product as $item){
+                            //
+                            // }
+
+                            // Output product information here
+
+
+                        endwhile;
+
+                    endif;
+
+                    wp_reset_query();
 
                 ?>
             </div>
